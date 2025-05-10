d42103b3e6dc702aa09394fd28487c78333a2768
Adoo Darko Jonathan <adoojonathan412@gmail.com>
2025-05-10 11:09:00 +0000

    updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,59 +1,122 @@
 import Link from 'next/link';
 
 const Footer = () => {
+  const buildOnEcoChain = [
+    {
+      title: 'Docs',
+      href: '/docs',
+    },
+    {
+      title: 'Mainnet Hub',
+      href: '/mainnet-hub',
+    },
+    {
+      title: 'Testnet Hub',
+      href: '/testnet-hub',
+    },
+    {
+      title: 'EcoChain Portal',
+      href: '/ecochain-portal',
+    },
+  ]
+
+  const ecochain = [
+    {
+      title: 'Bridge',
+      href: '/bridge',
+    },
+    {
+      title: 'Relay Bridge',
+      href: '/relay-bridge',
+    },
+    {
+      title: 'Ecosystem Fund',
+      href: '/ecosystem-fund',
+    },
+    {
+      title: 'Telegram',
+      href: '/telegram',
+    },
+    {
+      title: 'Twitter / X',
+      href: '/twitter-x',
+    },
+  ]
+
+  const community = [
+    {
+      title: 'Discord',
+      href: '/discord',
+    },
+    {
+      title: 'Forum',
+      href: '/forum',
+    },
+    {
+      title: 'Blog',
+      href: '/blog',
+    },
+  ]
+
   return (
     <div className="flex flex-col bg-gradient-to-bl from-[#032a1c] via-black to-[#032a1c] px-4 pt-12 pb-0">
       <div className="mx-auto w-full flex flex-col items-center">
         <div className="flex flex-col md:flex-row justify-end items-start gap-12 lg:gap-42 w-full lg:px-32 pb-4">
           <div className="text-white uppercase">
-            <h1 className="font-manuka text-2xl md:text-3xl">Build on ecochain</h1>
+            <h1 className="text-2xl md:text-3xl">Build on ecochain</h1>
             <ul className="flex flex-col gap-2 mt-3 text-sm">
-              <li className="hover:underline">
-                <Link href="/">Docs</Link>
-              </li>
-              <li className="hover:underline">
-                <Link href="/">Mainnet Hub</Link>
-              </li>
-              <li className="hover:underline">
-                <Link href="/">Testnet Hub</Link>
-              </li>
-              <li className="hover:underline">
-                <Link href="/">EcoChain Portal</Link>
-              </li>
+              {buildOnEcoChain.map((item) => (
+                <li key={item.title} className="group">
+                  <Link href={item.href}>
+                    <div className="relative overflow-hidden">
+                      <div className="transition-transform duration-500 group-hover:-translate-y-full hover:underline">
+                        {item.title}
+                      </div>
+                      <div className="absolute top-0 left-0 transition-transform duration-500 translate-y-full hover:underline group-hover:translate-y-0">
+                        {item.title}
+                      </div>
+                    </div>
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
           <div className="text-white uppercase">
-            <h1 className="font-manuka text-2xl md:text-3xl">ecochain</h1>
+            <h1 className="text-2xl md:text-3xl">ecochain</h1>
             <ul className="flex flex-col gap-2 mt-3 text-sm">
-              <li className="hover:underline">
-                <Link href="/">Bridge</Link>
-              </li>
-              <li className="hover:underline">
-                <Link href="/">Relay Bridge</Link>
-              </li>
-              <li className="hover:underline">
-                <Link href="/">Ecosystem Fund</Link>
-              </li>
-              <li className="hover:underline">
-                <Link href="/">Telegram</Link>
-              </li>
-              <li className="hover:underline">
-                <Link href="/">Twitter / X</Link>
-              </li>
+              {ecochain.map((item) => (
+                <li key={item.title} className="group">
+                  <Link href={item.href}>
+                    <div className="relative overflow-hidden">
+                      <div className="transition-transform duration-500 group-hover:-translate-y-full hover:underline">
+                        {item.title}
+                      </div>
+                      <div className="absolute top-0 left-0 transition-transform duration-500 translate-y-full hover:underline group-hover:translate-y-0">
+                        {item.title}
+                      </div>
+                    </div>
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
           <div className="text-white uppercase">
-            <h1 className="font-manuka text-2xl md:text-3xl">Community</h1>
+            <h1 className="text-2xl md:text-3xl">Community</h1>
             <ul className="flex flex-col gap-2 mt-3 text-sm">
-              <li className="hover:underline">
-                <Link href="/">Discord</Link>
-              </li>
-              <li className="hover:underline">
-                <Link href="/">Forum</Link>
-              </li>
-              <li className="hover:underline">
-                <Link href="/">Blog</Link>
-              </li>
+              {community.map((item) => (
+                <li key={item.title} className="group">
+                  <Link href={item.href}>
+                    <div className="relative overflow-hidden">
+                      <div className="transition-transform duration-500 group-hover:-translate-y-full hover:underline">
+                        {item.title}
+                      </div>
+                      <div className="absolute top-0 left-0 transition-transform duration-500 translate-y-full hover:underline group-hover:translate-y-0">
+                        {item.title}
+                      </div>
+                    </div>
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
@@ -67,11 +130,29 @@ const Footer = () => {
 
       <div className="mx-auto w-full pb-6">
         <div className="flex flex-col md:flex-row justify-between items-center gap-4 uppercase text-sm text-white opacity-80">
-          <div>2025 © EcoChain. All rights reserved.</div>
+          <div>© 2025 EcoChain. All rights reserved.</div>
           <div className="flex items-center gap-4">
-            <Link href="/privacy-policy" className="hover:underline">Privacy Policy</Link>
+            <Link href="/privacy-policy" className="group">
+              <div className="relative overflow-hidden">
+                <div className="transition-transform duration-500 group-hover:-translate-y-full hover:underline">
+                  Privacy Policy
+                </div>
+                <div className="absolute top-0 left-0 transition-transform duration-500 translate-y-full hover:underline group-hover:translate-y-0">
+                  Privacy Policy
+                </div>
+              </div>
+            </Link>
             <span>|</span>
-            <Link href="/terms-of-service" className="hover:underline">Terms of Service</Link>
+            <Link href="/terms-of-service" className="group">
+              <div className="relative overflow-hidden">
+                <div className="transition-transform duration-500 group-hover:-translate-y-full hover:underline">
+                  Terms of Service
+                </div>
+                <div className="absolute top-0 left-0 transition-transform duration-500 translate-y-full hover:underline group-hover:translate-y-0">
+                  Terms of Service
+                </div>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -115,10 +115,10 @@ export default function Hero() {
       <div className={`h-screen w-full overflow-hidden ${currentBg} transition-colors duration-500`}>
          <div className="absolute inset-0 bg-black opacity-80 pointer-events-none z-0"></div>
          <div className="wrapper">
-            <div className="scene w-full md:w-[1275px] h-[1020px] z-10"> {/* Reduced by 15% */}
+            <div className="scene w-full md:w-[1275px] h-[1020px] z-10">
                <div className="carousel keen-slider mt-16" ref={sliderRef}>
                   {slides.map((slide, index) => (
-                     <div key={index} className="carousel__cell w-[1250px] h-[765px]"> {/* Reduced by 15% */}
+                     <div key={index} className="carousel__cell w-[1250px] h-[765px]">
                         <div className="slide-content">
                            <img src={slide.image} alt={slide.title} className="slide-image" />
                         </div>
@@ -128,41 +128,41 @@ export default function Hero() {
             </div>
 
             <div className="z-20 rotate-z-[6deg] absolute top-0 left-0 w-full h-full flex flex-col justify-end items-start pointer-events-auto">
-               <div className="absolute bottom-10 left-4 md:left-10 lg:left-40 text-white">
-                  <h2 className="text-5xl md:text-6xl lg:text-7xl xl:text-9xl">{slides[currentSlide].title}</h2>
-                  <p className="text-xs sm:text-sm uppercase pt-2 pb-4 md:pb-8">{slides[currentSlide].description}</p>
-                  <div className="relative w-fit button-hover flex justify-center group">
-                     <span className="ping-button"></span>
-                     <span className="ping-button-2"></span>
-                     <button className="px-3 py-2 md:px-4 md:py-3 bg-white text-xs md:text-sm border-2 hover:border-4 transition-all duration-300 border-[#00EE7D] bg-white text-black rounded-xl uppercase">
-                        <div className="relative overflow-hidden">
-                           <div className="transition-transform duration-500 group-hover:-translate-y-full">
-                              {/*{slides[currentSlide].buttonText}*/}
-                              explore
+               <div className="hero-bottom-content flex md:flex-row flex-col md:justify-between md:items-center p-4 md:p-8 w-full">
+                  <div className="hero-info">
+                     <h2 className="text-5xl md:text-6xl lg:text-7xl xl:text-9xl">{slides[currentSlide].title}</h2>
+                     <p className="text-xs sm:text-sm uppercase pt-2 pb-4 md:pb-8">{slides[currentSlide].description}</p>
+                     <div className="relative w-fit button-hover flex justify-center group mx-auto md:mx-0">
+                        <span className="ping-button"></span>
+                        <span className="ping-button-2"></span>
+                        <button className="px-3 py-2 md:px-4 md:py-3 bg-white text-xs md:text-sm border-2 hover:border-4 transition-all duration-300 border-[#00EE7D] bg-white text-black rounded-xl uppercase">
+                           <div className="relative overflow-hidden">
+                              <div className="transition-transform duration-500 group-hover:-translate-y-full">
+                                 explore
+                              </div>
+                              <div className="absolute top-0 left-0 transition-transform duration-500 translate-y-full group-hover:translate-y-0">
+                                 explore
+                              </div>
                            </div>
-                           <div className="absolute top-0 left-0 transition-transform duration-500 translate-y-full group-hover:translate-y-0">
-                              {/*{slides[currentSlide].buttonText}*/}
-                              explore
-                           </div>
-                        </div>
-                     </button>
+                        </button>
+                     </div>
                   </div>
-               </div>
-               <div className="absolute flex gap-2 md:gap-4 bottom-4 md:bottom-10 right-4 md:right-10 lg:right-40">
-                  {slides.map((slide, index) => (
-                     <img
-                        key={index}
-                        src={slide.image}
-                        alt={`Thumbnail ${slide.title}`}
-                        className={`object-cover opacity-60 bg-cover bg-center border-3 rounded-lg w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 ${index === currentSlide ? 'border-[#00EE7D] opacity-100' : 'border-transparent'}`}
-                        onClick={() => handleThumbnailClick(index)}
-                     />
-                  ))}
+                  <div className="hero-thumbnails pr-8">
+                     {slides.map((slide, index) => (
+                        <img
+                           key={index}
+                           src={slide.image}
+                           alt={`Thumbnail ${slide.title}`}
+                           className={`object-cover opacity-60 bg-cover bg-center border-3 rounded-lg w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 ${index === currentSlide ? 'border-[#00EE7D] opacity-100' : 'border-transparent'}`}
+                           onClick={() => handleThumbnailClick(index)}
+                        />
+                     ))}
+                  </div>
                </div>
             </div>
 
             {/* Navigation Buttons */}
-            <div className="absolute rotate-z-[6deg] right-4 md:right-10 top-2/3 transform -translate-y-1/2 z-20 flex flex-col gap-2 md:gap-4">
+            <div className="absolute rotate-z-[6deg] right-4 md:right-10 top-1/2 transform -translate-y-1/2 z-20 flex flex-col gap-2 md:gap-4">
                <button
                   onClick={() => instanceRef.current?.prev()}
                   className="p-2 md:p-3 rounded-full border-3 hover:bg-[#00EE7D]/70 transition-all duration-300 text-white"
@@ -232,12 +232,42 @@ export default function Hero() {
                object-fit: cover;
             }
 
+            .hero-bottom-content {
+               position: absolute;
+               bottom: 10px;
+               left: 1rem;
+               color: white;
+            }
+            .hero-info {
+               text-align: left;
+            }
+            .hero-thumbnails {
+               display: flex;
+               gap: 0.5rem;
+               justify-content: flex-start;
+            }
             @media (max-width: 768px) {
-               .wrapper {
-                  transform: rotateZ(-3deg);
+               .hero-bottom-content {
+                  left: 0;
+                  right: 0;
+                  width: 100vw;
+                  align-items: center;
+                  text-align: center;
+                  bottom: 1.5rem;
                }
-               .z-20 {
-                  transform: rotateZ(3deg);
+               .hero-info {
+                  width: 100%;
+                  text-align: center;
+                  align-items: center;
+                  justify-content: center;
+                  display: flex;
+                  flex-direction: column;
+               }
+               .hero-thumbnails {
+                  justify-content: center;
+                  width: 100%;
+                  margin-top: 1.5rem;
+                  margin-bottom: 0.5rem;
                }
             }
 
